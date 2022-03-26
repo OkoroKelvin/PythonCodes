1486703920e963b8946c36ed8ae7644166d69070
OkoroKelvin <okorokelvinemoakpooke@yahoo.com>
2022-03-26 08:08:41 +0100

    Updated Python

---
+++ intermediate_training/lambda/my_lambda.py
@@ -0,0 +1,16 @@
+# lambda arguments: expression
+
+add10 = lambda x: x + 10
+print(add10(5))
+
+
+# The above function is written as
+def add10_func(x):
+    return x + 10
+
+
+mult = lambda x, y: x * y
+print(mult(5, 4))
+
+points2D: list
+points2D = [(1,2),(115,1)]
